5c040642f348201fda7a25bcec749f7c7388881d
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-30 14:15:16 +0200

    Update TechtileScope.py

--- src/TechtileScope.py
+++ src/TechtileScope.py
@@ -20,6 +20,12 @@ class Scope:
     """
 
     def __init__(self, ip: str, mode: ScopeMode = ScopeMode.POWER, config=None) -> None:
+        if ip is None:
+            if config is None or 'ip' not in config:
+                print("Please provide or an IP address or a config with an IP address specified (key = ip)")
+                exit()
+            else:
+                ip = config["ip"]
         self.visa_address = f"TCPIP::{ip}::INSTR"
 
         self.span = None
