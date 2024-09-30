550e567c613d1c2ca3de10a44da971c951e3be59
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-30 14:36:27 +0200

    Update TechtileScope.py

--- src/TechtileScope.py
+++ src/TechtileScope.py
@@ -50,62 +50,128 @@ class Scope:
     def query(self, s: str):
         return self.scope.query(s)
 
+    def get_data(self) -> float:
+        return self.scope.query_binary_values("CURVe?", datatype='d', container=np.array)
+
     def setup(self, bandwidth, center, span, rbw):
 
         self.span = span
         self.rbw = rbw
 
-        # TODO make this configurable
         rm = visa.ResourceManager()
         self.scope = rm.open_resource(self.visa_address)
         self.scope.timeout = 10000  # ms
         self.scope.encoding = "latin_1"
         self.scope.read_termination = "\n"
         self.scope.write_termination = None
-        self.write("*cls")  # clear ESR
-        self.write("*rst")  # reset
-        _ = self.query("*opc?")  # sync
+        self.scope.write("*cls")  # clear ESR
+        self.scope.write("*rst")  # reset
+        r = self.scope.query("*opc?")  # sync
+
+        self.scope.query("*idn?")
 
         # Channel 1 50Ohm 2GHz
-        self.write("CH1:TERMINATION 50")
-        self.write(f"CH1:BANDWIDTH {bandwidth}")
+        self.scope.write("CH1:TERMINATION 50")
+        self.scope.write(f"CH1:BANDWIDTH {bandwidth}")
 
-        # open spectrum view
+        self.scope.query(":CH1:TERMINATION?")
+        self.scope.query(":CH1:BANdwidth?")
+
+        # Open spectrum view
         # spectrum view 910MHz and 100kHz BW
-        self.write("DISplay:SELect:SPECView1:SOUrce CH1")
-        self.write("CH1:SV:STATE ON")
-        self.write(f"CH1:SV:CENTERFrequency {center}")
-        self.write(f"SV:SPAN {span}")
+        self.scope.write("DISplay:SELect:SPECView1:SOUrce CH1")
+        self.scope.write("CH1:SV:STATE ON")
+        self.scope.write(f"CH1:SV:CENTERFrequency {center}")
+        self.scope.write(f"SV:SPAN {self.span}")
 
-        self.write("SV:RBWMode MANUAL")
-        self.write("SV:RBW {self.rbw}")
+        self.scope.write("SV:RBWMode MANUAL")
+        self.scope.write(f"SV:RBW {self.rbw}")
 
-        self.write("SV:CH1:UNIts DBM")
+        self.scope.write("SV:CH1:UNIts DBM")
 
-        self.write("DATa:SOUrce CH1_SV_NORMal")
+        self.scope.write("DATa:SOUrce CH1_SV_NORMal")
+
+        self.scope.query("DATa:SOUrce?")
 
         data_stop = round(self.span / self.rbw * 2)
 
-        self.write("DATa:START 1")
-        self.write(f"DATa:STOP {data_stop}")  # 1901
+        self.scope.write("DATa:START 1")
+        self.scope.write(f"DATa:STOP {data_stop}")
 
-        self.write("WFMOutpre:BYT_Or LSB")
+        self.scope.query("DATa:STARt?")
+        self.scope.query("DATa:STOP?")
 
-        _ = self.query("*opc?")  # sync
-        print(self.query("*IDN?"))
+        time.sleep(2)
 
-    def get_power_dBm(self, cable_loss=None) -> float:
-        if cable_loss is None:
-            cable_loss = 0
-            print("no cabling loss taken into account")
-        pwr_dbm = self.scope.query_binary_values(
-            "CURVe?", datatype="d", container=np.array
-        )
-        pwr_lin = 10 ** (pwr_dbm / 10)
-        tot_pwr_dbm = float(
-            10 * np.log10(np.sum(pwr_lin))
-        )  # float to cast to single element
-        return tot_pwr_dbm + cable_loss
+        print(self.scope.query("WFMOutpre:SPAN?"))
+        self.scope.write("WFMOutpre:BYT_Or LSB")
+
+        self.scope.query("DATA:WIDTH?")
+        self.scope.query(":WFMOutpre?")
+
+        print(self.scope.query("WFMOutpre:NR_Pt?"))
+
+def calc_full_channel_power(self, data) -> float:
+        #pwr_lin = 10 ** (data / 10)
+        #tot_pwr_dbm = float(10*np.log10(np.sum(pwr_lin))) #float to cast to single element
+        #return tot_pwr_dbm + self.cable_loss
+
+        # Convert dBm to Watts
+        power_samples_W = 10**(data / 10) * 1e-3  # Convert dBm to Watts
+
+        # Integrate power samples over the frequency band (each sample corresponds to the RBW)
+        total_power_W = np.sum(power_samples_W)
+
+        # Convert total power back to dBm
+        total_power_dBm = 10 * np.log10(total_power_W / 1e-3)
+
+        return total_power_dBm
+
+        # print(f"Total channel power: {total_power_dBm:.2f} dBm")
+ 
+
+    def calc_channel_power_peaks(self, data, search_for_no_peaks) -> float:
+
+        #   Check span adjustments externally
+        self.check_span()
+
+        #   Get spectrum form scope
+        pwr_dbm = self.scope.query_binary_values("CURVe?", datatype='d', container=np.array)
+        
+        #   Calculate all peaks in spectrum
+        peaks,_ = find_peaks(pwr_dbm)
+        #print(pwr_dbm[peaks])
+
+        #   Sort peaks in descending order
+        peaks_sorted = sorted(pwr_dbm[peaks], reverse=True)
+        # print(peaks_sorted[:search_for_no_peaks])
+
+        #   Combine peaks to one overall power value
+        power_linear = 10 ** (np.asarray(peaks_sorted[:search_for_no_peaks]) / 10)
+        #print(power_linear)
+        tot_pwr_dbm = float(10*np.log10(np.sum(power_linear))) #float to cast to single element
+        return tot_pwr_dbm, peaks        
+
+
+    def check_span(self):
+        new_span = float(self.query(f"SV:SPAN?"))
+        if new_span != self.span:
+            
+            #   Warning
+            print("Span changed externally!")
+
+            #   Update span value
+            self.span = new_span
+
+            data_stop = round(self.span/self.rbw*2)
+
+            self.write(f"DATa:START 1")
+            self.write(f"DATa:STOP {data_stop}")#1901
+
+    def get_power_dBm(self) -> float:
+        pwr_data_dbm = self.get_data()
+        tot_pwr_dbm, peaks = self.calc_channel_power_peaks(pwr_data_dbm, 1)
+        return tot_pwr_dbm
 
     def get_power_dBm_peaks(self, search_for_no_peaks, cable_loss=None) -> float:
 
